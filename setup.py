1bc9e10ada2c7489d50f456b47cd7607ce45bce6
Travis Howse <tjhowse@gmail.com>
2020-10-11 14:39:36 +1000

    Version bump.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="modbus4mqtt",
-    version="0.3.0",
+    version="0.3.1",
     author="Travis Howse",
     author_email="tjhowse@gmail.com",
     description="A YAML-defined bidirectional Modbus to MQTT interface",
