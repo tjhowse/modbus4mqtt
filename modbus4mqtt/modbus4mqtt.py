47bd8e818c634b4aa7269dd1a6fc0901b3fa6611
Matthijs Kooijman <matthijs@stdin.nl>
2022-10-11 15:58:08 +0000

    Make the slave address configurable
    This allows a top-level "slave" entry in the config to set the slave
    address globally (defaulting to 1 as before), but also allows overriding
    the slave address for individual registers (which allows communicating
    with multiple slaves on the same bus).

--- modbus4mqtt/modbus4mqtt.py
+++ modbus4mqtt/modbus4mqtt.py
@@ -31,6 +31,7 @@ class mqtt_interface():
         self.prefix = mqtt_topic_prefix
         self.address_offset = self.config.get('address_offset', 0)
         self.registers = self.config['registers']
+        self.default_slave = self.config.get('slave', 1)
         for register in self.registers:
             register['address'] += self.address_offset
         self.modbus_connect_retries = -1  # Retry forever by default
@@ -67,7 +68,8 @@ class mqtt_interface():
             sleep(self.modbus_reconnect_sleep_interval)
         # Tells the modbus interface about the registers we consider interesting.
         for register in self.registers:
-            self._mb.add_monitor_register(register.get('table', 'holding'), register['address'], register.get('type', 'uint16'))
+            slave = register.get('slave', self.default_slave)
+            self._mb.add_monitor_register(register.get('table', 'holding'), slave, register['address'], register.get('type', 'uint16'))
             register['value'] = None
 
     def modbus_connection_failed(self):
@@ -105,6 +107,7 @@ class mqtt_interface():
         for register in self._get_registers_with('pub_topic'):
             try:
                 value = self._mb.get_value( register.get('table', 'holding'),
+                                            register.get('slave', self.default_slave),
                                             register['address'],
                                             register.get('type', 'uint16'))
             except Exception:
@@ -197,7 +200,8 @@ class mqtt_interface():
                               "Bad/missing value_map? Topic: {}, Value: {}".format(topic, value))
                 continue
             type = register.get('type', 'uint16')
-            self._mb.set_value(register.get('table', 'holding'), register['address'], int(value),
+            slave = register.get('slave', self.default_slave)
+            self._mb.set_value(register.get('table', 'holding'), slave, register['address'], int(value),
                                register.get('mask', 0xFFFF), type)
 
     # This throws ValueError exceptions if the imported registers are invalid
