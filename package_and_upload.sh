f4657bb25bfe09888ab02db1812133a81dd7f8f2
Pubaluba <30649275+Pubaluba@users.noreply.github.com>
2023-08-07 00:25:04 +0200

    Delete package_and_upload.sh

--- package_and_upload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-version=v`cat ./modbus4mqtt/version.py | cut -d\" -f2`
-rm ./dist/modbus4mqtt*.whl
-rm ./dist/modbus4mqtt*.tar.gz
-python3 setup.py sdist bdist_wheel
-python3 -m twine upload dist/*
-docker build -t tjhowse/modbus4mqtt:latest -t tjhowse/modbus4mqtt:"$version" .
-docker push tjhowse/modbus4mqtt:latest
-docker push tjhowse/modbus4mqtt:"$version"
